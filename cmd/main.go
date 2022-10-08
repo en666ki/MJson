1398d8a25cac568fa380d4bf464675673df4b67c
en666ki <chernyshovka.work@gmail.com>
2022-10-08 19:08:44 +0400

    figured out how to work with local files

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+    "github.com/en666ki/MJson/internal/json_reader"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    json_reader.Parse("string")
 }
---
+++ internal/json_reader/json_reader.go
@@ -0,0 +1,14 @@
+package json_reader
+
+import (
+//    "encoding/json"
+//    "log"
+    "fmt"
+)
+
+func Parse(input string) string {
+    fmt.Println(input)
+    return input
+}
+
+
